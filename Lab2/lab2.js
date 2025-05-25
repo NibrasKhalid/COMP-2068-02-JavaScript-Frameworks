11dc2dd3590b5f0b689357a687dc8eb509180d4c
NibrasKhalid <156461760+NibrasKhalid@users.noreply.github.com>
2025-05-25 13:30:03 -0400

    Used Switch case to check for win conditions

--- Lab2/lab2.js
+++ Lab2/lab2.js
@@ -8,15 +8,14 @@ prompt.start();
 //first param is an array of values we want
 //second param is a callback function that will be called when the user finishes entering values
 prompt.get(['choice'], function (err, result) {
+    
     //retrieve the value from the result object by name
     //name must match the one given in array
     let userChoice = result.choice;
     let computerChoice;
-    //do something with the value
-    console.log('You entered: ' + userChoice);
 
     //for RPS here you will generate computer selection, compare it with user selection and print the result
-    let random = Math.random();;
+    let random = Math.random();
     switch (random) {
         case random > 0 && random < 0.35:
             computerChoice = 'Paper';
@@ -25,8 +24,41 @@ prompt.get(['choice'], function (err, result) {
             computerChoice = 'Scissors';
             break;
         case random > 0.68 && random < 1.01:
-            computerChoice = 'rock';
+            computerChoice = 'Rock';
             break;
     }
+
+    //do something with the value
+    console.log("You entered: " + userChoice);
+    console.log("Computer chose: " + computerChoice);
+    
+    //Checking win conditions
+    switch (userChoice) {
+        case userChoice === computerChoice:
+            console.log("Its a tie!");
+            break;
+        case userChoice === "Rock" && computerChoice === "Scissors":
+            console.log("User win");
+            break;
+        case userChoice === "Scissors" && computerChoice === "Paper":
+            console.log("User win!");
+            break;
+        case userChoice === "Paper" && computerChoice === "Rock":
+            console.log("User win!");
+            break;
+        case userChoice === "Scissors" && computerChoice === "Rock":
+            console.log("Computer wins!");
+            break;
+        case userChoice === "Paper" && computerChoice === "Scissors":
+            console.log("Computer wins!");
+            break;
+        case userChoice === "Rock" && computerChoice === "Paper":
+            console.log("Computer wins!");
+            break;
+        default:
+            console.log("Invalid choice");
+    }
+
+
 })
 ;
